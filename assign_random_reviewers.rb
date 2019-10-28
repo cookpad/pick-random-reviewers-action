6cbfe4d96f7f2ec49c3a5ab3d184fe88bdead7f3
Prem Sichanugrist <s@sikac.hu>
2019-10-28 20:59:40 +0900

    Allow skipping busy users

--- assign_random_reviewers.rb
+++ assign_random_reviewers.rb
@@ -2,13 +2,13 @@
 
 require "json"
 require "net/http"
-require "uri"
+require "open-uri"
 
 GITHUB_ACCESS_TOKEN = ARGV[0]
 GITHUB_ORG_NAME = ARGV[1]
 GITHUB_TEAM_NAME = ARGV[2]
 NUMBER_OF_REVIEWERS = ARGV[3].to_i
-AVOID_BUSY_REVIEWER = ARGV[4]
+AVOID_BUSY_REVIEWER = ARGV[4] == "true"
 GITHUB_TEAMS_URL = "https://api.github.com/orgs/#{GITHUB_ORG_NAME}/teams"
 
 # Sync $stdout so we can output log message right away
@@ -54,10 +54,29 @@ end
 
 # Step 3: Pick random reviewer
 members.shuffle!
-reviewers = members.first(NUMBER_OF_REVIEWERS)
+reviewers = []
 
-reviewer_names = reviewers.
-                 map { |member| member["login"] }.
-                 join(", ").
-                 sub(/^(.+), (.+)$/, "\\1, and \\2")
+members.each do |member|
+  if reviewers.size < NUMBER_OF_REVIEWERS
+    if AVOID_BUSY_REVIEWER
+      print "   See if #{member['login']} is busy... "
+
+      # Currently, GitHub do not provide user's status via API, so we need to
+      # fetch the status via HTML page
+      user_page = URI(member["html_url"]).read
+
+      if user_page.include?("user-status-container-border-busy")
+        puts "YES."
+        break
+      else
+        puts "no."
+      end
+    end
+
+    reviewers << member
+  end
+end
+
+reviewer_names = reviewers.map { |member| member["login"] }.join(", ")
+reviewer_names.sub!(/^(.+), (.+)$/, "\\1, and \\2")
 puts "=> Picked #{reviewer_names} as reviewers."
