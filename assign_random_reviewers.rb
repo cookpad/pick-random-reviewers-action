2e4cf0f8d885be524c3b9314a4af06bbb4a08ac4
Prem Sichanugrist <s@sikac.hu>
2019-10-28 17:46:24 +0900

    Download a list of teams in an organization

--- assign_random_reviewers.rb
+++ assign_random_reviewers.rb
@@ -1,5 +1,31 @@
 # frozen_string_literal: true
+
+require "json"
+require "open-uri"
+
 GITHUB_ACCESS_TOKEN = ARGV[0]
-GITHUB_TEAM = ARGV[1]
-NUMBER_OF_REVIEWERS = ARGV[2]
-AVOID_BUSY_REVIEWER = ARGV[3]
+GITHUB_ORG_NAME = ARGV[1]
+GITHUB_TEAM_NAME = ARGV[2]
+NUMBER_OF_REVIEWERS = ARGV[3]
+AVOID_BUSY_REVIEWER = ARGV[4]
+GITHUB_TEAMS_URL = "https://api.github.com/orgs/#{GITHUB_ORG_NAME}/teams"
+
+# Sync $stdout so we can output log message right away
+$stdout.sync = true
+
+def download_json(url)
+  JSON.parse(URI(url + "?access_token=#{GITHUB_ACCESS_TOKEN}").read)
+end
+
+# Step 1: Download list of teams to find its ID
+print "=> Downloading a list of teams for #{GITHUB_ORG_NAME}... "
+teams = download_json(GITHUB_TEAMS_URL)
+matching_team = teams.detect { |team| team["name"] == GITHUB_TEAM_NAME }
+
+if matching_team.nil?
+  puts "ERROR!"
+  puts "ERROR: Unable to find matching team. Exiting."
+  exit 10
+else
+  puts "done!"
+end
